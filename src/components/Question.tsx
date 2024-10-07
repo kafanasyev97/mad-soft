7a19df2f032e2514203b246a841fcef13dbd4cd9
Kirill <kirillmas222@gmail.com>
2024-10-08 02:16:39 +0400

    [upd] Main layout

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -5,14 +5,47 @@ import {
   FormGroup,
   Radio,
   RadioGroup,
+  TextField,
 } from '@mui/material'
 import { defaultValues } from '../defaultValues'
+import { useState } from 'react'
+
+type Answer = {
+  [key: number | string]: any
+}
 
 const Question = ({ activeStep }: { activeStep: number }) => {
+  const [answers, setAnswers] = useState<Answer>({})
+  console.log(answers)
+
+  const handleChange = (value: string | string[], id: number | string) => {
+    setAnswers((prev) => ({
+      ...prev,
+      [id]: value,
+    }))
+  }
+
+  const handleCheckboxChange = (option: string, id: number | string) => {
+    const currentAnswers = answers[id] || []
+    const updatedAnswers = currentAnswers.includes(option)
+      ? currentAnswers.filter((item: string) => item !== option)
+      : [...currentAnswers, option]
+
+    setAnswers((prev) => ({
+      ...prev,
+      [id]: updatedAnswers,
+    }))
+  }
+
   const renderQuestion = (type: string) => {
     if (type === 'single') {
       return (
-        <RadioGroup>
+        <RadioGroup
+          value={answers[defaultValues[activeStep].id] || ''}
+          onChange={(e) =>
+            handleChange(e.target.value, defaultValues[activeStep].id)
+          }
+        >
           {defaultValues[activeStep].options?.map((elem) => (
             <div>
               <FormControlLabel
@@ -35,15 +68,34 @@ const Question = ({ activeStep }: { activeStep: number }) => {
                 value={elem}
                 control={<Checkbox />}
                 label={elem}
+                onChange={() =>
+                  handleCheckboxChange(elem, defaultValues[activeStep].id)
+                }
               />
             </div>
           ))}
         </FormGroup>
       )
     } else if (type === 'short') {
-      return <input />
+      return (
+        <TextField
+          fullWidth
+          value={answers[defaultValues[activeStep].id] || ''}
+          onChange={(e) =>
+            handleChange(e.target.value, defaultValues[activeStep].id)
+          }
+        />
+      )
     } else {
-      return <textarea />
+      return (
+        <TextField
+          fullWidth
+          value={answers[defaultValues[activeStep].id] || ''}
+          onChange={(e) =>
+            handleChange(e.target.value, defaultValues[activeStep].id)
+          }
+        />
+      )
     }
   }
   return (
